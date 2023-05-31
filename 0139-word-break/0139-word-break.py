6fa328db5dcda69a78105b7b98dfea616c742f31
wnsdy95 <wnsdy98@gmail.com>
2023-05-30 23:56:12 -0400

    Time: 40 ms (11.98%), Space: 16.6 MB (7.14%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,29 @@
+
+    
+def fun(s,dc,memo):
+    if(memo[s]==1):
+        return False
+    if(dc[s]==1):
+        return True
+    for i in range(len(s)+1):
+        # print(dc)
+        # print(memo)
+        if(dc[s[:i]]==1):
+            a=fun(s[i:],dc,memo)
+            if(a):
+                return True
+    memo[s]=1
+    return False
+class Solution:
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dc=defaultdict(lambda:0)
+        memo=defaultdict(lambda:0)
+        for w in wordDict:
+            dc[w]=1
+        return(fun(s,dc,memo))
+        
