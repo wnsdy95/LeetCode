16fcb4166a74c926377704ae570042569d75c7c9
wnsdy95 <wnsdy98@gmail.com>
2023-06-14 00:40:37 -0400

    Time: 1066 ms (5.00%), Space: 22.7 MB (15.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,31 +4,31 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-#         if len(prices) == 1:
-#             return 0
-#         dp = [0]*len(prices)
-#         for i in range(len(prices)):
-#             if i == 0:
-#                 dp[0] = prices[0]
+        if len(prices) == 1:
+            return 0
+        dp = [0]*len(prices)
+        for i in range(len(prices)):
+            if i == 0:
+                dp[0] = prices[0]
                 
-#             elif i > 0:
-#                 if dp[0] >= prices[i]:
-#                     dp[0] = prices[i]
-#                     if i > 1:
-#                         dp[i] = max(dp[i-1], dp[i])
+            elif i > 0:
+                if dp[0] >= prices[i]:
+                    dp[0] = prices[i]
+                    if i > 1:
+                        dp[i] = dp[i-1]
                     
-#                 elif dp[0] < prices[i]:
-#                     if i == 1:
-#                         dp[i] = prices[i] - dp[0]
-#                     elif i > 1:
-#                         dp[i] = max(prices[i] - dp[0], dp[i-1])
+                elif dp[0] < prices[i]:
+                    if i == 1:
+                        dp[i] = prices[i] - dp[0]
+                    elif i > 1:
+                        dp[i] = max(prices[i] - dp[0], dp[i-1])
                         
-#         return dp[-1]
-        buy = prices[0]
-        profit = 0
-        for i in prices[1:]:
-            if i - buy > 0:
-                profit = max(profit, i - buy)
-            else:
-                buy = i
-        return profit
+        return dp[-1]
+        # buy = prices[0]
+        # profit = 0
+        # for i in prices[1:]:
+        #     if i - buy > 0:
+        #         profit = max(profit, i - buy)
+        #     else:
+        #         buy = i
+        # return profit
